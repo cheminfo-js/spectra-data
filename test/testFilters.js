313751fdfdebf2979b32a6adfef831429f556a79
andcastillo <andcastillo@gmail.com>
2016-04-27 11:25:40 -0500

    Debugging  code removed

--- test/testFilters.js
+++ test/testFilters.js
@@ -20,46 +20,20 @@ describe('spectra-data test Filters', function () {
     //console.log(peakPicking);
     it('fourier Tranformation', function () {
         var spectrum=createSpectraData("/data/fftTest/FID.dx");
-        //console.log(spectrum.spectra);
-        //spectrum.fourierTransform();
-        console.log(spectrum.getNbPoints());
-        spectrum.digitalFilter({nbPoints:-67}).fourierTransform();
-        //spectrum.zeroFilling(spectrum.getNbPoints()*2).digitalFilter({nPoints:67}).fourierTransform();
-        spectrum.phaseCorrection(Math.PI/2, 0);
-        spectrum.setActiveElement(1);
-        //console.log(JSON.stringify(spectrum.getXData()));
-        //console.log(JSON.stringify(spectrum.getYData()));
-        for(var i=0;i<15;i++){
-            console.log(spectrum.getX(i));
-        }
-        for(var i=0;i<15;i++){
-            //console.log(spectrum.getY(i));
-        }
-        console.log(" ");
-        spectrum.setActiveElement(0);
-        //console.log(JSON.stringify(spectrum.getXData()));
-        //console.log(JSON.stringify(spectrum.getYData()));
-        for(var i=0;i<15;i++){
-            console.log(spectrum.getX(i));
-        }
-        for(var i=0;i<15;i++){
-           // console.log(spectrum.getY(i));
-        }
-        console.log(JSON.stringify((spectrum.getVector(spectrum.getFirstX(),spectrum.getLastY(),1024))[0]));
         //console.log(spectrum.getNbPoints());
-        //spectrum.zeroFilling(spectrum.getNbPoints()*2).digitalFilter({nPoints:67}).fourierTransform();
-        //
+        //spectrum.digitalFilter({nbPoints:-67}).fourierTransform();
+        spectrum.zeroFilling(spectrum.getNbPoints()*2).digitalFilter({nPoints:67}).fourierTransform();
+        spectrum.phaseCorrection(-Math.PI/2, 0);
+        //console.log(JSON.stringify((spectrum.getVector(spectrum.getFirstX(),spectrum.getLastY(),1024))[1]));
         spectrum.getXUnits().should.equal("PPM");
-        console.log(spectrum.getFirstX()+" "+spectrum.getLastX());
-        console.log(spectrum.getX(0)+" "+spectrum.getX(spectrum.getNbPoints()-1));
 
     });
     it('zeroFilling nbPoints', function () {
         var spectrum=createSpectraData("/data/fftTest/FID.dx");
         spectrum.zeroFilling(10).getNbPoints().should.equal(10);
         spectrum.zeroFilling(20).getNbPoints().should.equal(20);
-        console.log(spectrum.getFirstX()+" "+spectrum.getLastX());
-        console.log(spectrum.getX(0)+" "+spectrum.getX(spectrum.getNbPoints()-1));
+        //console.log(spectrum.getFirstX()+" "+spectrum.getLastX());
+        //console.log(spectrum.getX(0)+" "+spectrum.getX(spectrum.getNbPoints()-1));
     });
 });
 
