1ed7c0243a0dbb66ee455596db30edb3345ed271
andres castillo <andcastillo@gmail.com>
2016-06-14 18:55:31 +0200

    Removed the local ACS parser and use the nmr-formater instead

--- test/testACS.js
+++ test/testACS.js
@@ -25,7 +25,7 @@ describe('spectra-data test peak picking in ACS format', function () {
     });
 
     it('format ACS new input format', function () {
-        var acs = Data.ACS2.toACS(peakPicking2,{rangeForMultiplet:true, nucleus:spectrum.getNucleus(), observe:spectrum.observeFrequencyX()});
+        var acs = Data.ACS.toACS(peakPicking2,{rangeForMultiplet:true, nucleus:spectrum.getNucleus(), observe:spectrum.observeFrequencyX()});
         acs.should.equal("<sup>1</sup>H NMR (400 MHz) δ 7.26-7.32 (m, 2 H), 7.15-7.23 (m, 3 H), 2.60 (q, 2 H, <i>J</i> = 7.6 Hz), 1.19 (t, 3 H, <i>J</i> = 7.6 Hz).");
     });
 });
