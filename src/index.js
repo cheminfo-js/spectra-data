4b906e352f4a2e4992184e4858eb7e28d4b9d3d3
andres castillo <andcastillo@gmail.com>
2016-06-14 18:56:27 +0200

    Change the package identifier from ACS to formater

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 exports.SD = require('./SD');
 exports.NMR = require('./NMR');
 exports.NMR2D = require('./NMR2D');
-exports.ACS = require('nmr-formater');
+exports.formater = require('nmr-formater');
 //exports.ACS2 = require('./AcsParserNew');
 exports.JAnalyzer = require('./JAnalyzer');
 //exports.SD2 = require('/SD2');
--- test/testACS.js
+++ test/testACS.js
@@ -20,12 +20,12 @@ describe('spectra-data test peak picking in ACS format', function () {
     var peakPicking = spectrum.nmrPeakDetection({"nH":10, realTop:true, thresholdFactor:0.8,clean:true,compile:true});
     var peakPicking2 = spectrum.nmrPeakDetection({"nH":10, realTop:true, thresholdFactor:0.8,clean:true,compile:true,format:"new"});
     it('format ACS', function () {
-        var acs = Data.ACS.toACS(peakPicking,{rangeForMultiplet:true});
+        var acs = Data.formater.toACS(peakPicking,{rangeForMultiplet:true});
         acs.should.equal("<sup>1</sup>H NMR (400 MHz) δ 7.26-7.32 (m, 2 H), 7.15-7.23 (m, 3 H), 2.60 (q, 2 H, <i>J</i> = 7.6 Hz), 1.19 (t, 3 H, <i>J</i> = 7.6 Hz).");
     });
 
     it('format ACS new input format', function () {
-        var acs = Data.ACS.toACS(peakPicking2,{rangeForMultiplet:true, nucleus:spectrum.getNucleus(), observe:spectrum.observeFrequencyX()});
+        var acs = Data.formater.toACS(peakPicking2,{rangeForMultiplet:true, nucleus:spectrum.getNucleus(), observe:spectrum.observeFrequencyX()});
         acs.should.equal("<sup>1</sup>H NMR (400 MHz) δ 7.26-7.32 (m, 2 H), 7.15-7.23 (m, 3 H), 2.60 (q, 2 H, <i>J</i> = 7.6 Hz), 1.19 (t, 3 H, <i>J</i> = 7.6 Hz).");
     });
 });
