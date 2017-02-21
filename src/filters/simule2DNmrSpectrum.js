8ee2817dd791d5441e1dc5a73dba5cbec379c330
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-02-21 18:33:20 -0500

    fix lineshape

--- src/filters/simule2DNmrSpectrum.js
+++ src/filters/simule2DNmrSpectrum.js
@@ -9,20 +9,25 @@ var defaultOptions = {
 
 function simule2DNmrSpectrum(table, options) {
     var i, j;
+    // console.log(table)
     const fromLabel = table[0].fromAtomLabel;
     const toLabel = table[0].toLabel;
-    const resolution = options.resolution || 3;
     const freqResonanceCarbon = options.frequencyX || 100;
     const freqResonanceProton = options.frequencyY || 400;
-    var lineWidthX = options.lineWidthX  || 8;
-    var lineWidthY = options.lineWidthY  || 12;
+    var lineWidthX = options.lineWidthX  || 20;
+    var lineWidthY = options.lineWidthY  || 4;
+    const resolutionX = lineWidthX / 2;
+    const resolutionY = lineWidthY / 2
     const addX = 5;
-    const addY = 0.5
-    lineWidthX = lineWidthX / freqResonanceCarbon;
-    lineWidthY = lineWidthY / freqResonanceProton;
+    const addY = 3;
+    lineWidthX = lineWidthX / freqResonanceCarbon ;
+    lineWidthY = lineWidthY / freqResonanceProton ;
     // console.log("lineWidthX ", lineWidthX)
     // console.log("lineWidthY ", lineWidthY)
 
+    if ((options.firstY !== undefined && options.lastY !== undefined)) {
+        min
+    }
     i = 1;
     var minX = table[0].fromChemicalShift,
         maxX = table[0].fromChemicalShift,
@@ -35,6 +40,7 @@ function simule2DNmrSpectrum(table, options) {
         } else if (maxX < table[i].fromChemicalShift) {
             maxX = table[i].fromChemicalShift;
         }
+
         if(minY > table[i].toChemicalShift) {
             minY = table[i].toChemicalShift;
         } else if (maxY < table[i].toChemicalShift) {
@@ -42,61 +48,61 @@ function simule2DNmrSpectrum(table, options) {
         }
         i += 1;
     }
-    // console.log("minMax of X and Y")
-    // console.log(minX, maxX)
-    // console.log(minY, maxY)
+
     var nbPointsX = options.nbPointsX || Math.round(2 * (maxX - minX + 2 * addX) *
-            freqResonanceCarbon / resolution);
+            freqResonanceCarbon / resolutionX);
 
     var nbPointsY = options.nbPointsY || Math.round(2 * (maxY - minY + 2 * addY) *
-            freqResonanceProton / resolution);
+            freqResonanceProton / resolutionY);
 
 
     var transpose = false;
     var homoNuclear = false;
+    var parameters;
     if (fromLabel === toLabel) {
         homoNuclear = true;
-        var parameters = {
-            nbPointsX: nbPointsX,
+        parameters = {
+            nbPointsX: nbPointsY,
             fromToX: [minX - addY, maxX + addY],
             fromToY: [minY - addY, maxY + addY],
             nbPointsY: nbPointsY,
-            sigmaX: lineWidthX,
+            sigmaX: lineWidthY,
             sigmaY: lineWidthY,
-        }
+        };
     } else {
         if(fromLabel === "H") {
-            var parameters = {
+            parameters = {
                 nbPointsX: nbPointsY,
                 nbPointsY: nbPointsX,
                 fromToX: [minY - addX, maxY + addX],
                 fromToY: [minX - addY, maxX + addY],
-                sigmaX: lineWidthY,
-                sigmaY: lineWidthX,
-            }
+                sigmaX: lineWidthX,
+                sigmaY: lineWidthY,
+            };
             transpose = true;
         } else {
-            var parameters = {
+            parameters = {
                 nbPointsX: nbPointsX,
                 nbPointsY: nbPointsY,
                 fromToX: [minX - addX, maxX + addX],
                 fromToY: [minY - addY, maxY + addY],
                 sigmaX: lineWidthX,
                 sigmaY: lineWidthY,
-            }
+            };
         }
     }
 
-    console.log(parameters.nbPointsX, parameters.nbPointsY)
     var spectraMatrix = new Matrix(parameters.nbPointsY, parameters.nbPointsX).fill(0);
 
+    var xVector = range(parameters.fromToX[0], parameters.fromToX[1], parameters.nbPointsX);
+    var yVector = range(parameters.fromToY[0], parameters.fromToY[1], parameters.nbPointsY);
 
     i = 0;
     while(i < table.length) {
         parameters.couplingConstant = table[i].j;
         parameters.pathLength = table[i].pathLength;
         if(transpose) {
-            parameters.integral = table[i].toAtoms.length;
+            parameters.integral = table[i].fromAtoms.length;
             parameters.fromChemicalShift = table[i].toChemicalShift;
             parameters.toChemicalShift = table[i].fromChemicalShift;
         } else {
@@ -104,13 +110,14 @@ function simule2DNmrSpectrum(table, options) {
             parameters.fromChemicalShift = table[i].fromChemicalShift;
             parameters.toChemicalShift = table[i].toChemicalShift;
         }
-        addPeak(spectraMatrix, parameters);
+        addPeak(spectraMatrix, xVector, yVector, parameters);
         i += 1;
     }
     return spectraMatrix;
 }
 
-function addPeak(matrix, parameters) {
+function addPeak(matrix, xVector, yVector, parameters) {
+
     const fromToX = parameters.fromToX;
     const fromToY = parameters.fromToY;
     const X = parameters.fromChemicalShift;
@@ -118,33 +125,26 @@ function addPeak(matrix, parameters) {
     const couplingConstant = parameters.couplingConstant;
     const nbPointsX = parameters.nbPointsX;
     const nbPointsY = parameters.nbPointsY;
-    const sigmaX = parameters.sigmaX;
-    const sigmaY = parameters.sigmaY;
+    const squareSigmaX = Math.pow(parameters.sigmaX, 2) * 2;
+    const squareSigmaY = Math.pow(parameters.sigmaY, 2) * 2;
     const integral = parameters.integral;
-    // console.log("integral")
-    // console.log(integral);
 
     var xIndex = getIndex(X, fromToX[0], fromToX[1], nbPointsX);
     var yIndex = getIndex(Y, fromToY[0], fromToY[1], nbPointsY);
-    // console.log("X and Y")
-    // console.log(X, Y)
-    var yMax = Y + sigmaY * 2;
-    var yMaxIndex = getIndex(yMax, fromToY[0], fromToY[1], nbPointsY);
-    var yVector = range(Y, yMax, Math.abs(yIndex - yMaxIndex + 1));
 
-    var xMax = X + sigmaX * 2;
+    var yMax = Y + parameters.sigmaY * 4;
+    var yMaxIndex = getIndex(yMax, fromToY[0], fromToY[1], nbPointsY);
+
+    var xMax = X + parameters.sigmaX * 4;
     var xMaxIndex = getIndex(xMax, fromToX[0], fromToX[1], nbPointsX);
-    var xVector = range(X, xMax, Math.abs(xIndex - xMaxIndex + 1));
-    // console.log("Index x and y")
-    // console.log(xMaxIndex, xIndex)
-    // console.log(yMaxIndex, yIndex)
-    for (var i = 0; i < Math.abs(xIndex - xMaxIndex + 1); i++) {
-        for(var j = 0; j < Math.abs(yIndex - yMaxIndex + 1); j++) {
-            var result = Math.exp(
-                    -1 * (Math.pow(xVector[i] - X, 2) /(2 * sigmaX * sigmaX) +
-                    Math.pow(yVector[j] - Y, 2) /(2 * sigmaY * sigmaY))
-                ) / (2 * 3.1416 * sigmaX * sigmaY) * 1000 * integral;
-            // console.log(result);
+
+    for (var j = 0; j < Math.abs(yIndex - yMaxIndex + 1); j++) {
+        for (var i = 0; i < Math.abs(xIndex - xMaxIndex + 1); i++) {
+
+            var exponent = Math.pow(xVector[xIndex + i] - X, 2) / squareSigmaX +
+                Math.pow(yVector[yIndex + j] - Y, 2) / squareSigmaY;
+            var result = integral / Math.exp(exponent);
+
             matrix[yIndex + j][xIndex + i] += result;
             matrix[yIndex - j][xIndex + i] += result;
             matrix[yIndex + j][xIndex - i] += result;
